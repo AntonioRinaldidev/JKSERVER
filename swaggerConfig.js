f4486dbf9bfe48b0152a852f110770b4c67dd4e9
Antonio <a.rinaldi.dev@gmail.com>
2025-03-13 03:16:32 +0100

    swagger changes

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -2,26 +2,26 @@ const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API",
-      version: "1.0.0",
-      description: "JKSERVER API documentation",
-    },
-    servers: [
-      {
-        url: "http://localhost:4000", // Update with your actual base URL
-      },
-    ],
-  },
-  apis: ["./Routes/*.js"], // Path to the API routes for documentation
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API",
+			version: "1.0.0",
+			description: "JKSERVER API documentation",
+		},
+		servers: [
+			{
+				url: "https://157.90.158.44:8000/", // Update with your actual base URL
+			},
+		],
+	},
+	apis: ["./Routes/*.js"], // Path to the API routes for documentation
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
 const setupSwagger = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 module.exports = setupSwagger;
